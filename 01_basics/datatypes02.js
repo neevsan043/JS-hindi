138420b7e5f53532dde31ffa912ad2753627dbab
neevsan043 <163330109+neevsan043@users.noreply.github.com>
2024-06-10 09:20:40 +0000

    datatypes done

---
+++ 01_basics/datatypes02.js
@@ -0,0 +1,23 @@
+"use strict"; //treats all the code as per the new js version
+
+//alert("Hello") // we are using node js,not browser
+console.log(3 + 3);
+console.log("Neev");
+
+let name = "Neev"
+let age = 18
+let isLoggedin = false
+
+// number =   2 to the power 53 
+// bigInt 
+// string => ""
+// boolean =>  true / false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // object
---
+++ 01_basics/variables1.js
@@ -0,0 +1,16 @@
+const acountId = 223433
+let acountEmail = "neev@gmail.com"
+var acountPassword = "1234"
+/*
+Do not use var to assign a variable name 
+*/
+acountCity = "Jaipur"
+let acountState;
+// acountId = 2  constant values can't be changed
+console.log(acountId); 
+acountEmail = "hc@hc.com"
+acountPassword = "12121"
+acountCity = "uttrakhand"
+
+console.table([acountId,acountEmail,acountPassword,acountCity,acountState])
+ 
