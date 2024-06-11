8bfcb66a54ad9244b51fa127cf3494ed6780fb9f
neevsan043 <163330109+neevsan043@users.noreply.github.com>
2024-06-11 15:30:34 +0000

    date started

---
+++ 01_basics/datesinjs07.js
@@ -0,0 +1,10 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+console.log(myDate.toISOString()); 
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(2024,5,11)
+console.log(myCreatedDate.toDateString());
