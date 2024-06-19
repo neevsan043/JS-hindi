53aa3fc93722341bb7e2c97207f1da58ab8dc62d
neevsan043 <163330109+neevsan043@users.noreply.github.com>
2024-06-19 18:34:56 +0000

    change

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,11 +1,11 @@
 // const Newarr = ['marvel','daniel','nobi','samanta']
 // console.log(Newarr);
 // const arr = [1,2,3,4]
-// const deaf = [...Newarr ,...arr]
+// const deaf = [...Newarr ,...arr] spreading 
 // console.log(deaf);
 
 // Unpacking arrays into a single array 
-//  use  .flat(Infinity)
+//  use  .flat(Infinity) concat to join to arrays 
 
 console.log(Array.isArray("Neev"));
 console.log(Array.from("Neev"));
